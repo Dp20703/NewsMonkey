8d35d8b0d78cf1775a21e666efe9c2fa70d8cb57
Darshan <darshanrp2003@gmail.com>
2024-05-26 14:43:02 +0530

    Sticky Navbar & bug fixes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-    render() {
-        let { title, description, imgUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className='my-3'>
-                <div className="card">
-                    <div>
-                <span className="d-flex justify-content-end  position-absolute   badge rounded-pill bg-danger" style={{right:'0'}}>  {source}</span>
 
-                    </div>
-                    <img src={imgUrl ? imgUrl : "https://images.moneycontrol.com/static-mcnews/2024/05/20240520181216_Feat-sec.jpg"} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
-                        <p className="card-text">{description}</p>
-                        <a rel="noreferrer" href={newsUrl} target='_blank' className="btn  btn-sm btn-dark">Read More</a>
-                    </div>
+    let { title, description, imgUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className='my-3'>
+            <div className="card">
+                <div>
+                    <span className="d-flex justify-content-end  position-absolute   badge rounded-pill bg-danger" style={{ right: '0' }}>  {source}</span>
+
+                </div>
+                <img src={imgUrl ? imgUrl : "https://images.moneycontrol.com/static-mcnews/2024/05/20240520181216_Feat-sec.jpg"} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
+                    <p className="card-text">{description}</p>
+                    <a rel="noreferrer" href={newsUrl} target='_blank' className="btn  btn-sm btn-dark">Read More</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default NewsItem
