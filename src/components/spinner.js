1efe28feba75fff4bce2f347b96f90528b6b6bf1
Darshan <darshanrp2003@gmail.com>
2024-06-12 13:36:09 +0530

    Changes in Spinner

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import loading from './loading.gif'
+// import loading from './loading.gif'
 
 const spinner = () => {
     return (
         <div className="text-center my-3">
-            <img src={loading} alt="loading"  style={{backgroundColor:"black"}}/>
+            {/* <img src={loading} alt="loading"  style={{backgroundColor:"black"}}/> */}
+
+            <div class="spinner-border text-primary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+
         </div>
     )
 
