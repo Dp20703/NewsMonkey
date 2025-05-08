49468b243e5afa0e85695b8a37f06f897797cafb
dp <codewithdp2073@gmail.com>
2025-05-08 14:37:04 +0530

    Fix: Use optional chaining for articles length in InfiniteScroll component

--- src/pages/News.js
+++ src/pages/News.js
@@ -86,7 +86,7 @@ const News = (props) => {
             {loading && <Spinner />}
 
             <InfiniteScroll
-                dataLength={articles.length}
+                dataLength={articles?.length}
                 next={fetchMoreData}
                 hasMore={articles.length !== totalResults}
                 loader={<Spinner />}>
